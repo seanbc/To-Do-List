0f7ce9c3ec198f46ce9350fcd24620d29fcfe398
Sean Costello <seanbc07@gmail.com>
2019-07-07 12:21:01 +0800

    Added JS function so duplicates cannot be entered

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var enterButton = document.getElementById('enter');
 var input = document.getElementById('userInput') ;
 var ul = document.querySelector('ul');
 var listItem = document.getElementsByTagName('li');
+var holder = [];
+
+
 
 function inputLength(){
   return input.value.length;
@@ -11,10 +14,14 @@ function liLength() {
   return listItem.length;
 }
 
+
+
 function createListElement() {
   var li = document.createElement('li');
+
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+
   input.value = "";
 
   function itemDone() {
@@ -29,21 +36,45 @@ function createListElement() {
        li.appendChild(button2);
        button2.addEventListener("click", deleteItem);
 
+      // function arrayRemove() {
+      //
+      //   for( var i = 0; i < holder.length; i++){
+      //      if ( arr[i] === deleteValue) {
+      //        arr.splice(i, 1);
+      //        i--;
+      //      }
+      //   }
+      // }
+
        function deleteItem() {
-               li.classList.add('delete');
+          li.classList.add('delete');
+
      }
 
 }
 
+
+
+function checkRepition(){
+var newInput = input.value;
+    if (holder.indexOf(newInput) > -1) {
+      alert("You added that one already!");
+    } else {
+    holder.push(input.value);
+    createListElement();
+    }
+  }
+
+
 function addItemAfterClick() {
   if (inputLength() > 0) {
-     createListElement();
+     checkRepition();
    }
 }
 
 function addItemAfterEnter() {
   if (inputLength() > 0 && event.which ===13) {
-     createListElement();
+    checkRepition();
    }
 }
 
